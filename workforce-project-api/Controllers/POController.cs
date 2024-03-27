83f156fbf3573f18a1cbf21f18e00375a9a2789c
loren <111726817+lorennnzzoo@users.noreply.github.com>
2024-03-27 12:08:14 +0530

    added industry and PO controllers

---
+++ workforce-project-api/Controllers/POController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace workforce_project_api.Controllers
+{
+    public class POController : ApiController
+    {
+    }
+}
